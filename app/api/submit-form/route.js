cedc67141ba65c4f0482583dc9c0a88a093316d2
Nikhil Sharma <94921824+NikhilCode12@users.noreply.github.com>
2024-05-31 15:14:10 +0530

    Update route.js

--- app/api/submit-form/route.js
+++ app/api/submit-form/route.js
@@ -6,20 +6,21 @@ const MONGO_URI = process.env.MONGO_URL;
 
 export async function POST(request) {
   try {
-    const conn = mongoose.connect(MONGO_URI);
-    if (conn) {
-      const data = await request.json();
-      const newStudent = new Student(data);
-      newStudent.save();
-      return NextResponse.json(
-        {
-          message: "Student registered successfully!",
-        },
-        {
-          status: 200,
-        }
-      );
-    }
+    await mongoose.connect(MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+    const data = await request.json();
+    const newStudent = new Student(data);
+    await newStudent.save();
+    return NextResponse.json(
+      {
+        message: "Student registered successfully!",
+      },
+      {
+        status: 200,
+      }
+    );
   } catch (err) {
     return NextResponse.json({ error: err });
   }
