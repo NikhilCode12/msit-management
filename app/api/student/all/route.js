9a7ec2bed19d8986f617fc1e9c3613e4da07d06b
Pulkit-Koshal <113760450+Pulkit-Koshal@users.noreply.github.com>
2024-06-10 23:51:18 +0530

    Update route.js

--- app/api/student/all/route.js
+++ app/api/student/all/route.js
@@ -5,9 +5,9 @@ import Student from "@/lib/model/student";
 export async function GET() {
   try {
     await ConnectToDatabase();
-    console.log(Student.count())
-    const students = await Student.find({});
-    console.log(Student.count())
+    // console.log(Student.count())
+    const students = await Student.find({}).count();
+    // console.log(Student.count())
     if (!students || students.length === 0) {
       return NextResponse.json({ message: "No Record Found" });
     }
