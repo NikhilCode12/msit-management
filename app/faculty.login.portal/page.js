ef0bfe476146d3da801a56b2f8953cb56e09d248
Shaan Goswami <93421552+notshaangoswami@users.noreply.github.com>
2024-06-23 14:50:18 +0530

    Update page.js

--- app/faculty.login.portal/page.js
+++ app/faculty.login.portal/page.js
@@ -74,13 +74,20 @@ export default function FacultyPortal() {
 
   const fetchStudentData = useCallback(async () => {
     try {
-      const response = await fetch("/api/student/all", {
-        headers: {
+      // const response = await fetch("/api/student/all", {
+      //   headers: {
+      //   "Cache-Control": "no-cache",
+      //   "Pragma": "no-cache",
+      //   "Expires": "0",
+      // },
+      // });
+      const response = await axios.get("/api/student/all", {
+      headers: {
         "Cache-Control": "no-cache",
         "Pragma": "no-cache",
         "Expires": "0",
       },
-      });
+    });
       //console.log('response rec')
       const data = await response.json();
       //setData(tdata);
