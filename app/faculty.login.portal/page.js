144bc42ed5f52fd5737c09aa7ee3c85a69ca5356
Shaan Goswami <93421552+notshaangoswami@users.noreply.github.com>
2024-06-23 13:28:38 +0530

    Update page.js

--- app/faculty.login.portal/page.js
+++ app/faculty.login.portal/page.js
@@ -89,7 +89,7 @@ export default function FacultyPortal() {
       console.error("Error fetching student data:", error);
       return [];
     }
-  }, []);
+  }, [data]);
 
   const handleAccessList = async (e) => {
     e.preventDefault();
